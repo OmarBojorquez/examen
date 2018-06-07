76f04750e1dcba44cd8c9a90baad7445358341ce
OmarBojorquez <dgpe.curso.16@gmail.com>
2018-06-07 17:42:43 -0500

    Agregue tests y ruta Docker en el README

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Repo para el examen Calculadora 7 Jun
 Este proyecto funciona como una calculadora básica
 
 Para probar su funcionamiento se sugiere acudir al DockerHub correspondiente
-y seguir las instrucciones: XXX
+y seguir las instrucciones: https://hub.docker.com/r/omarbojorquez/calcu/
--- src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
+++ src/test/java/mx/unam/dgpe/sample/controller/TestMyController.java
@@ -17,7 +17,6 @@ public class TestMyController extends AbstractVerticle {
         assertTrue("Este es un mensaje", result.length() >1);
 	logger.info(result);
 
-
         result = sendGet("http://0.0.0.0:8080/api/tercero?mode=gusgus2");
         assertTrue("Este es un mensaje", result.length() >1);
         logger.info(result);
@@ -26,7 +25,21 @@ public class TestMyController extends AbstractVerticle {
         assertTrue("Este es un mensaje", result.length() >1);
         logger.info(result);
 
+	result = sendGet("http://0.0.0.0:8081/api/suma?num1=5&num2=9");
+        assertTrue("Fallo suma", result.length() > 1);
+        logger.info(result);
 
+        result = sendGet("http://0.0.0.0:8081/api/resta?num1=9&num2=3");
+        assertTrue("Fallo resta", result.length() > 1);
+        logger.info(result);
+
+	result = sendGet("http://0.0.0.0:8081/api/multiplicacion?num1=2&num2=7");
+        assertTrue("Fallo multiplicacion", result.length() > 1);
+        logger.info(result);
+
+        result = sendGet("http://0.0.0.0:8081/api/division?num1=9&num2=3");
+        assertTrue("Fallo division", result.length() > 1);
+        logger.info(result);
 
     }
 
